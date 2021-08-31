c9f5e77fa391b9b4931bd3d39274f40ced4f14ce
Alexander Brooks <alexanderlawbrooks@gmail.com>
2021-08-31 17:58:59 -0400

    fixed auth btns logic

--- server/routers/search.js
+++ server/routers/search.js
@@ -13,22 +13,38 @@ async function getAllMovies(url) {
 }
 
 router.get('/movies', (req, res) => {
-  res.render('search');
+  if (req.session) {
+    if (req.session.user_id) {
+      res.locals.isAuthenticated = true;
+    }
+  }
+  res.render('search', { user_id: req.session.user_id });
 });
 
 // server-side search - returns object for mustache template
 router.post('/movies', async (req, res) => {
+  if (req.session) {
+    if (req.session.user_id) {
+      res.locals.isAuthenticated = true;
+    }
+  }
   const searchTerm = req.body.search;
   const url = `https://www.omdbapi.com/?apikey=${process.env.API_KEY}&s=${searchTerm}&page=1&type=%22movie%22`;
   const searchedMovies = await getAllMovies(url);
   res.render('search', {
     movies: searchedMovies.Search,
-    searchTerm: searchTerm
+    searchTerm: searchTerm,
+    user_id: req.session.user_id
   });
 });
 
 // async search - returns json
 router.get('/movies/:searchTerm', async (req, res) => {
+  if (req.session) {
+    if (req.session.user_id) {
+      res.locals.isAuthenticated = true;
+    }
+  }
   const searchTerm = req.params.searchTerm;
   const url = `https://www.omdbapi.com/?apikey=${process.env.API_KEY}&s=${searchTerm}&page=1&type=%22movie%22`;
   const searchedMovies = await getAllMovies(url);
